4e6aed87f2b346893bf867150072559328eaef83
PlugInPlaya <tsanders1114@gmail.com>
2018-04-15 12:31:54 -0400

    LandingPage Creation

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -16,6 +16,35 @@ class Homepage extends Component {
           alt="indiecall"
           className="indiecall-img"
           />
+          <div className="banner-text">
+          <h1>Explore</h1>
+
+          <hr/>
+
+          <p>Independent Creators Works From Around The Globe</p>
+          <p>Music Works|Film Works|Motivational Works|Visual Works</p>
+
+          <div className="explore-links">
+          {/*Music Link To Explore Page*/}
+          <a href="http://google.com" rel="noopener noreferrer"target="_blank">
+            <i className="fas fa-music" aria-hidden="true"/>
+          </a>
+          {/*Film Link To Explore Page*/}
+          <a href="http://google.com" rel="noopener noreferrer"target="_blank">
+            <i className="fas fa-film" aria-hidden="true"/>
+          </a>
+          {/*Motivation Link To Explore Page*/}
+          <a href="http://google.com"rel="noopener noreferrer" target="_blank">
+            <i className="fas fa-child" aria-hidden="true"/>
+           </a>
+            {/*Visual Link To Explore Page*/}
+            <a href="http://google.com"rel="noopener noreferrer" target="_blank">
+           <i className="fas fa-eye" aria-hidden="true"/>
+           </a>
+           
+
+          </div>
+          </div>
       </Cell>
      </Grid>
      </div>
